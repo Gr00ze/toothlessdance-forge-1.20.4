1b5fe4c1d13ad63bab9f6a88ca95fe9bea88a5c4
Gr00ze <vincenzop23@gmail.com>
2024-01-06 00:19:55 +0100

    Breeding now generate a new toothless

--- src/main/java/com/gr00ze/toothlessmeme/entity/ToothlessEntity.java
+++ src/main/java/com/gr00ze/toothlessmeme/entity/ToothlessEntity.java
@@ -2,17 +2,22 @@ package com.gr00ze.toothlessmeme.entity;
 
 import com.gr00ze.toothlessmeme.client.sounds.Sounds;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.Cat;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import static com.gr00ze.toothlessmeme.ToothlessMemeMod.MOD_ID;
 
@@ -39,6 +44,7 @@ public class ToothlessEntity extends Cat {
                 .build();
     }
 
+
     @Override
     public void tick() {
         super.tick();
@@ -60,15 +66,20 @@ public class ToothlessEntity extends Cat {
     @Override
     public @NotNull InteractionResult mobInteract(@NotNull Player p_28153_, @NotNull InteractionHand p_28154_) {
         ItemStack itemstack = p_28153_.getItemInHand(p_28154_);
-        //System.out.println("Interazione");
         if (this.isFood(itemstack) && !isDancing){
             this.playSound(Sounds.DANCE.get());
+
             isDancing = true;
-            //System.out.println("Partita");
         }
-//        float volume = 1,pitch = 1F;
-//        this.level().playLocalSound(this.getX(),this.getY(),this.getZ(), Sounds.DANCE.get(),this.getSoundSource(),volume,pitch,false);
 
         return super.mobInteract(p_28153_, p_28154_);
+    }
+
+
+    @Nullable
+    @Override
+    public ToothlessEntity getBreedOffspring(@NotNull ServerLevel serverLevel, @NotNull AgeableMob p_148871_) {
+        ToothlessEntity toothlessEntity = EntityInit.TOOTHLESS_ENTITY.get().create(serverLevel);
+        return toothlessEntity;
     }
 }
