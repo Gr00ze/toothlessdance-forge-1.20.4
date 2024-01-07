93a988eb8a23e1376f7b63837f45a0144fc76e76
Gr00ze <vincenzop23@gmail.com>
2024-01-07 21:14:33 +0100

    Added Animations Class

---
+++ src/main/java/com/gr00ze/toothlessmeme/client/animations/Animations.java
@@ -0,0 +1,10 @@
+package com.gr00ze.toothlessmeme.client.animations;
+
+import net.minecraft.client.animation.AnimationDefinition;
+
+
+public class Animations {
+    public static final AnimationDefinition TOOTHLESS_DANCE = AnimationDefinition.Builder.withLength(18).build();
+    public static final AnimationDefinition TOOTHLESS_EATING = AnimationDefinition.Builder.withLength(3).build();
+
+}
