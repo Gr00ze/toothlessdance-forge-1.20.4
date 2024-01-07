cb7b620091bc89253c354184002a31ec15daaa1c
Gr00ze <vincenzop23@gmail.com>
2024-01-07 03:24:38 +0100

    Added spawnegg

--- src/main/java/com/gr00ze/toothlessmeme/items/ItemsInitializer.java
+++ src/main/java/com/gr00ze/toothlessmeme/items/ItemsInitializer.java
@@ -2,6 +2,7 @@ package com.gr00ze.toothlessmeme.items;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -12,7 +13,7 @@ import static com.gr00ze.toothlessmeme.entity.EntityInit.TOOTHLESS_ENTITY;
 public class ItemsInitializer {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    //public static final RegistryObject<Item> TOOTHLESS_EGG_ITEM = ITEMS.register("toothless_egg", () -> new SpawnEggItem(TOOTHLESS_ENTITY.get(),123123,12313123, new Item.Properties()));
+    public static final RegistryObject<Item> TOOTHLESS_EGG_ITEM = ITEMS.register("toothless_egg", () -> new ForgeSpawnEggItem(TOOTHLESS_ENTITY,0x111111,0x111A1A, new Item.Properties()));
 
     public static void register(IEventBus e){
         ITEMS.register(e);
