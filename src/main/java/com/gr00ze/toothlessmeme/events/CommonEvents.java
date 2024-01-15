8d882521c1991abfb5bc18a3fb0363431501d0ae
gr00ze <vincenzop23@gmail.com>
2024-01-15 19:46:33 +0100

    Added on creative tab spawn eggs

--- src/main/java/com/gr00ze/toothlessmeme/events/CommonEvents.java
+++ src/main/java/com/gr00ze/toothlessmeme/events/CommonEvents.java
@@ -3,13 +3,17 @@ package com.gr00ze.toothlessmeme.events;
 import com.gr00ze.toothlessmeme.entity.EntityInit;
 import com.gr00ze.toothlessmeme.entity.ToothlessEntity;
 import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import static com.gr00ze.toothlessmeme.ToothlessMemeMod.MOD_ID;
+import static com.gr00ze.toothlessmeme.items.ItemsInitializer.TOOTHLESS_SPAWN_EGG;
 
 @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CommonEvents {
@@ -19,12 +23,22 @@ public class CommonEvents {
     }
     @SubscribeEvent
     public static void registerSpawnPlacement(SpawnPlacementRegisterEvent event){
+        System.out.println("THINK BOWSER THINK");
         event.register(
                 EntityInit.TOOTHLESS_ENTITY.get(),
                 SpawnPlacements.Type.ON_GROUND,
                 Heightmap.Types.WORLD_SURFACE,
                 ToothlessEntity::canSpawn,
                 SpawnPlacementRegisterEvent.Operation.OR);
+    }
+    @SubscribeEvent
+    public static void buildContents(BuildCreativeModeTabContentsEvent event) {
+        System.out.println("THINK MARK THINK");
+        if (event.getTabKey() == CreativeModeTabs.SPAWN_EGGS) {
+            event.accept(TOOTHLESS_SPAWN_EGG.get());
+
+
+        }
     }
 
 
--- src/main/java/com/gr00ze/toothlessmeme/items/ItemsInitializer.java
+++ src/main/java/com/gr00ze/toothlessmeme/items/ItemsInitializer.java
@@ -1,5 +1,7 @@
 package com.gr00ze.toothlessmeme.items;
 
+import net.minecraft.world.flag.FeatureFlag;
+import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 import net.minecraftforge.common.ForgeSpawnEggItem;
@@ -13,7 +15,7 @@ import static com.gr00ze.toothlessmeme.entity.EntityInit.TOOTHLESS_ENTITY;
 public class ItemsInitializer {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<Item> TOOTHLESS_EGG_ITEM = ITEMS.register("toothless_spawn_egg", () -> new ForgeSpawnEggItem(TOOTHLESS_ENTITY,0x111111,0x111A1A, new Item.Properties()));
+    public static final RegistryObject<Item> TOOTHLESS_SPAWN_EGG = ITEMS.register("toothless_spawn_egg", () -> new ForgeSpawnEggItem(TOOTHLESS_ENTITY,0x111111,0x111A1A, new Item.Properties().stacksTo(64)));
 
     public static void register(IEventBus e){
         ITEMS.register(e);
